b6410ec2c77b15ffa7c4761bf1f563e6e6a77fe8
oa1cspace <oa1cspace@gmail.com>
2022-07-27 17:56:05 +0300

    feat: added TS

---
+++ src/users/user.ts
@@ -0,0 +1,16 @@
+import express, { NextFunction, Request, Response } from 'express';
+
+export const userRouter = express.Router();
+
+userRouter.use((req: Request, res: Response, next: NextFunction) => {
+    console.log('user router');
+    next();
+});
+
+userRouter.use('/register', (req: Request, res: Response) => {
+    res.send('register');
+});
+
+userRouter.use('login', (req: Request, res: Response) => {
+    res.send('login');
+});
