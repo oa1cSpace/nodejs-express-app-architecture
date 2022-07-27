08006decaf7bef2908a978c41579844d7e428f57
oa1cspace <oa1cspace@gmail.com>
2022-07-27 19:21:51 +0300

    feat: logger & simple DI

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Express } from 'express';
 import { userRouter } from './users/user';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service';
  
  export class App {
     app: Express;
     server: Server;
     port: number;
+    logger: LoggerService;
 
-    constructor() {
+    constructor(logger: LoggerService) {
         this.app = express();
         this.port = 8000;
+        this.logger = logger;
     };
 
     userRoutes() {
@@ -19,6 +22,6 @@ import { Server } from 'http';
     public async init() {
         this.userRoutes();
         this.server = this.app.listen(this.port);
-        console.info(`Server up & running on: http://localhost:${ this.port }`);
+        this.logger.log(`Server up & running on: http://localhost:${ this.port }`)
     }
  }
